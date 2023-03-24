3597280fe8ac6b104ec56e19fc83dca46a03917f
marmez11 <116234002+marmez11@users.noreply.github.com>
2023-03-24 00:40:03 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const { response } = require('express')
 // Database Connection
 /////////////////////////////////////////////
 // Setup inputs for our connect function
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = "mongodb+srv://mrz11:681ByAkUu0KADW4N@sei.zkwdpc1.mongodb.net/finance_crypto?retryWrites=true&w=majority"
 const CONFIG = {
     useNewUrlParser: true,
     useUnifiedTopology: true
